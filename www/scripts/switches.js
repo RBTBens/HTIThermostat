8f312b0c75a2fb20f42678cc9460ca4122abf38f
Robert van Lente <robertvanlente@hotmail.com>
2016-06-14 12:51:33 +0200

    Added XML import

--- www/scripts/switches.js
+++ www/scripts/switches.js
@@ -9,8 +9,11 @@ var days = {
 	Su: 6
 }
 
+var daysfull = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
 // Variables
 var SWITCH_WIDTH;
+var INNER_WIDTH;
 var SWITCH_COUNT = 10;
 
 var EDITING = false;
@@ -19,11 +22,69 @@ var EDITING = false;
 var URL_DAY = "images/scheduler/sun.png";
 var URL_NIGHT = "images/scheduler/moon.png";
 
+// Sets SWITCH_WIDTH and INNER_WIDTH
+function setWidths() {
+	SWITCH_WIDTH = $(".switch-inner:first").height() / 4;
+	INNER_WIDTH = $(".switch-inner:first").width();
+}
+
+// Convert position to time
+function ptot(pos) {
+	return (pos/INNER_WIDTH)*24;
+}
+
+// Convert time to position
+function ttop(time) {
+	var p = time.indexOf(':');
+	var h = parseInt(time.substring(0, p));
+	var m = parseInt(time.substring(p+1, time.length));
+	
+	return (h/24 + m /1440) * INNER_WIDTH;
+}
+
+// Create switches from program
+function initSwitches(program) {
+	$("day", program).each(function(day) {
+		var inner = $(".switch-inner").eq(day);
+		
+		$("switch", this).each(function(i) {
+			if ($(this).attr("state") == "on") {
+				inner.append(createSwitch(ttop($(this).text())));
+			}
+		});
+    });
+	
+	update();
+}
+
+// Creates program from switches
+function saveSwitches() {
+	
+	var program = $("<schedule>");
+	
+	$(".switch-inner").each(function(day) {
+		
+		var day = $("<day>").attr("name",daysfull[day]);
+		
+		var sws = $(".switch", this);
+		
+		$(".switch", this).each(function() {
+			var sw = $("<switch>").attr("type", "").attr("state", "on");
+			sw.text(ptot($(this).position().left));
+			day.append(sw);
+		});
+		
+		program.append(day);
+	});
+	
+	// SEND TO SERVER
+}
+
 // Create a new switch
 function createSwitch(pos) {
 	// Create the element
 	var sw = $(document.createElement("div")).attr("class", "switch").append($(document.createElement("img")));
-	sw.css("left", pos).width(SWITCH_WIDTH);
+	sw.css("left", (((pos-(SWITCH_WIDTH/2))/INNER_WIDTH) * 100).toFixed(2) + '%').width(SWITCH_WIDTH);
 	
 	// Make sure the items aren't instantly removed after being spawned
 	setTimeout(function(){
@@ -43,7 +104,7 @@ function createSwitch(pos) {
 			}
 		});
 	}, 100);
-	
+
 	return sw;
 }
 
@@ -64,6 +125,70 @@ function createSwitchMenu(sw) {
 	m.append(rb);
 	
 	return m;
+}
+
+// When the document is ready
+$(document).ready(function(e) {
+	
+	// Fill up the container with all days
+	for (var day in days) {
+		var item = $("<li></li>");
+		item.append("<div class=\"day-container\"><div class=\"day\"><h1>" + day + "</h1></div></div>");
+		item.append("<div class=\"switch-container\"><div class=\"switch-inner\"></div></div>");
+		
+		$(".day-list").append(item);
+	}
+	
+	// Determine widths of containers
+	setTimeout(function() {
+		setWidths();
+		var xml = "<schedule><day name='Monday'><switch type='day' state='off'>0:00</switch><switch type='day' state='off'>00:00</switch><switch type='day' state='off'>00:00</switch><switch type='night' state='off'>00:00</switch><switch type='night' state='off'>00:00</switch><switch type='night' state='off'>00:00</switch><switch type='day' state='on'>7:00</switch><switch type='night' state='on'>8:00</switch><switch type='day' state='on'>12:00</switch><switch type='night' state='on'>14:00</switch></day><day name='Tuesday'><switch type='day' state='off'>0:00</switch><switch type='day' state='off'>00:00</switch><switch type='day' state='off'>00:00</switch><switch type='night' state='off'>00:00</switch><switch type='night' state='off'>00:00</switch><switch type='night' state='off'>00:00</switch><switch type='day' state='on'>7:00</switch><switch type='night' state='on'>8:00</switch><switch type='day' state='on'>12:00</switch><switch type='night' state='on'>14:00</switch></day></schedule>";
+		initSwitches($.parseXML(xml));
+	}, 1);
+		
+	// Also when sizes change
+	$(window).on("orientationchange", function(e) {
+		setTimeout(function() {setWidths();}, 100);
+	});
+	
+	// Go over the switches
+	$(".switch-inner").each(function(day) {
+		if (!app.isMobile)
+			$(this).on("click", function(e) {
+				if (!EDITING) onContainerClick(this, day, e); 
+			});
+		else 
+			$(this).on("doubletap", function(e) {
+				if (!EDITING) onContainerClick(this, day, e); 
+			});
+    });
+});
+
+// Switch container click handler
+function onContainerClick(container, day, e) {
+	var sws = $(".switch", container);
+	if (sws.length < SWITCH_COUNT) {
+		var pos = e.pageX - $(container).offset().left;
+		if (checkFree(day, pos) == true) {
+			if (sws.length > 0) {
+				var b = false;
+				sws.each(function() {
+					if (pos < $(this).position().left) {
+						b = true;
+						$(this).before(createSwitch(pos));
+						return false;
+					}
+				});
+				
+				if (!b)
+					$(container).append(createSwitch(pos));
+			} else {
+				$(container).append(createSwitch(pos));
+			}
+			
+			update();
+		}
+	}
 }
 
 // Check if we can actually place it here
@@ -91,61 +216,3 @@ function update() {
         });
     });
 }
-
-// Switch container click handler
-function onContainerClick(container, day, e) {
-	var sws = $(".switch", container);
-	if (sws.length < SWITCH_COUNT) {
-		var pos = e.pageX - $(container).offset().left - (SWITCH_WIDTH / 2);
-		if (checkFree(day, pos) == true) {
-			if (sws.length > 0) {
-				var b = false;
-				sws.each(function() {
-					if (pos < $(this).position().left) {
-						b = true;
-						$(this).before(createSwitch(pos));
-						return false;
-					}
-				});
-				
-				if (!b)
-					$(container).append(createSwitch(pos));
-			} else {
-				$(container).append(createSwitch(pos));
-			}
-			
-			update();
-		}
-	}
-}
-
-// Fill up the container with alll days
-for (var day in days) {
-	var item = $("<li></li>");
-	item.append("<div class=\"day-container\"><div class=\"day\"><h1>" + day + "</h1></div></div>");
-	item.append("<div class=\"switch-container\"><div class=\"switch-inner\"></div></div>");
-	
-	$(".day-list").append(item);
-}
-
-// When the document is ready
-$(document).ready(function(e) {
-	
-	// Determine switch width (4 = ratio height:width)
-	// Timeout fixes bug where height() is 0.
-	setTimeout(function() {
-		SWITCH_WIDTH = $(".switch-inner:first").height() / 4;
-	}, 1);
-	
-	// Go over the switches
-	$(".switch-inner").each(function(day) {
-		if (!app.isMobile)
-			$(this).on("click", function(e) {
-				if (!EDITING) onContainerClick(this, day, e); 
-			});
-		else 
-			$(this).on("doubletap", function(e) {
-				if (!EDITING) onContainerClick(this, day, e); 
-			});
-    });
-});
